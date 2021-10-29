978f793432bb76ce0cb86ff2824ec4a653991dd7
scottc11 <scott.m.campbell11@gmail.com>
2021-10-29 14:53:13 -0400

    adjusted clock frequency defines to be in hz

--- System/Inc/system_clock_config.h
+++ System/Inc/system_clock_config.h
@@ -2,12 +2,12 @@
 
 #include "stm32f4xx_hal.h"
 
-#define HCLK_FREQ 180
-#define SYSCLK_FREQ 180
-#define APB1_PERIPHERAL_FREQ 45
-#define APB1_TIM_FREQ 90
-#define APB2_PERIPHERAL_FREQ 90
-#define APB2_TIM_FREQ 180
+#define HCLK_FREQ 180000000            // 180 Mhz
+#define SYSCLK_FREQ 180000000          // 180 Mhz
+#define APB1_PERIPHERAL_FREQ 45000000  // 45 Mhz
+#define APB1_TIM_FREQ 90000000         // 90 Mhz
+#define APB2_PERIPHERAL_FREQ 90000000  // 90 Mhz
+#define APB2_TIM_FREQ 180000000        // 180 Mhz
 
 #ifdef __cplusplus
 extern "C"
