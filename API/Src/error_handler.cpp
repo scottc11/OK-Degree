ff5e5c0c989386dbb91bb7969184f43b3def8ad9
scottc11 <scott.m.campbell11@gmail.com>
2021-10-28 22:20:17 -0400

    error handler now locks system in a blink loop

--- API/Src/error_handler.cpp
+++ API/Src/error_handler.cpp
@@ -9,13 +9,33 @@ HAL_StatusTypeDef error_handler(HAL_StatusTypeDef error)
     switch (error)
     {
     case HAL_ERROR:
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+        while (1)
+        {
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+            HAL_Delay(500);
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+            HAL_Delay(500);
+        }
         break;
     case HAL_BUSY:
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+        while (1)
+        {
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+            HAL_Delay(500);
+            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+            HAL_Delay(500);
+        }
         break;
     case HAL_TIMEOUT:
         HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+        while (1)
+        {
+            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+            HAL_Delay(500);
+            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+            HAL_Delay(500);
+        }
         break;
     default:
         break;
