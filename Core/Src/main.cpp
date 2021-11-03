2b81af8105be349622b09722cfc17f84e5b5b7db
scottc11 <scott.m.campbell11@gmail.com>
2021-11-03 08:12:59 -0400

    cleaned up main()

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -13,73 +13,17 @@
 
 void mcpConfig();
 
-Flash flash;
-
-InterruptIn ctrlInt(PB_5); // pullup makes no difference
-
 PinName adcPins[8] = {ADC_A, ADC_B, ADC_C, ADC_D, PB_ADC_A, PB_ADC_B, PB_ADC_C, PB_ADC_D};
-
-DigitalOut led(PA_1);
-DigitalOut led2(PB_7);
-DigitalOut led3(PC_13);
 
 I2C i2c1(I2C1_SDA, I2C1_SCL, I2C::Instance::I2C_1);
 I2C i2c3(I2C3_SDA, I2C3_SCL, I2C::Instance::I2C_3);
 
-SX1509 io2(&i2c3);
-SX1509 io(&i2c3, 0x70);
-
-MPR121 pads(&i2c1, TOUCH_INT_A);
-MCP23017 mcp(&i2c1, MCP23017_CTRL_ADDR);
-DAC8554 dac(SPI2_MOSI, SPI2_SCK, DAC2_CS);
-
 SuperClock superClock;
 
-const int CHAN_LED_PINS[8] = {15, 14, 13, 12, 7, 6, 5, 4}; // io pin map for channel LEDs
-int thresholds[8] = { 8191, 16382, 24573, 32764, 40955, 49146, 57337, 65535 };
-uint8_t ioState;
-
-void toggleLED()
-{
-  led = !led.read();
-}
-bool btnPressed;
-void toggleLED2() {
-  led2 = !led2.read();
-  btnPressed = true;
-}
-
-void toggleLED3(uint8_t pad)
-{
-  led3 = !led3.read();
-}
 
 void ADC1_DMA_Callback(uint16_t values[])
 {
-  // create threshold array of 8 values divided by 65535
-  // take the incoming value, bit shift it, then map it to one of those 8 values by iterating over each value and
-  // seeing if it is less than i
-  int curr = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    uint16_t vall = convert12to16(values[0]);
-    if (vall < thresholds[i])
-    {
-      curr = i;
-      break;
-    }
-  }
-
-  for (int i = 0; i < 8; i++)
-  {
-    if (i == curr) {
-      // bit set
-      ioState = bitSet(ioState, i);
-      continue;
-    } else {
-      ioState = bitClear(ioState, i);
-    }
-  }
+  uint16_t val16 = convert12to16(values[0]);
 }
 
 int main(void)
@@ -98,75 +42,9 @@ int main(void)
 
   i2c1.init();
   i2c3.init();
-  io.init();
-  io2.init();
 
-  for (int i = 0; i < 8; i++)
-  {
-    io.ledConfig(CHAN_LED_PINS[i]);
-    io2.ledConfig(CHAN_LED_PINS[i]);
-  }
-
-  flash.erase(ADDR_FLASH_SECTOR_7);
-
-  uint32_t data[4] = { 10000, 25, 32677, 64987 };
-  flash.write(ADDR_FLASH_SECTOR_7, data, 4);
-
-  uint32_t buffer[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-  flash.read(ADDR_FLASH_SECTOR_7, buffer, 4);
-
-  ctrlInt.fall(callback(toggleLED2));
-  mcpConfig();
-  mcp.digitalReadAB();
-
-  pads.init();
-  pads.attachCallbackTouched(callback(toggleLED3));
-  pads.attachCallbackReleased(callback(toggleLED3));
-  pads.enable();
-
-  dac.init();
-  uint16_t value = 0;
   while (1)
   {
-    value += 50;
-    dac.write(DAC8554::Channels::CHAN_A, value);
-    dac.write(DAC8554::Channels::CHAN_B, value);
-    dac.write(DAC8554::Channels::CHAN_C, value);
-    dac.write(DAC8554::Channels::CHAN_D, value);
-    HAL_Delay(1);
 
-    if (btnPressed)
-    {
-      btnPressed = false;
-      mcp.digitalReadAB();
-    }
-    
-    pads.poll();
-
-    for (int i = 0; i < 8; i++)
-    {
-      int status = bitRead(ioState, i);
-      if (status)
-      {
-        io.analogWrite(CHAN_LED_PINS[i], 240);
-      }
-      else
-      {
-        io.analogWrite(CHAN_LED_PINS[i], 0);
-      }
-    }
-    
   }
-}
-
-void mcpConfig() {
-  mcp.init();
-  mcp.setDirection(MCP23017_PORTA, 0xff);
-  mcp.setDirection(MCP23017_PORTB, 0b01111111);
-  mcp.setInterupt(MCP23017_PORTA, 0xff);
-  mcp.setInterupt(MCP23017_PORTB, 0b01111111);
-  mcp.setPullUp(MCP23017_PORTA, 0xff);
-  mcp.setPullUp(MCP23017_PORTB, 0b01111111);
-  mcp.setInputPolarity(MCP23017_PORTA, 0xff);
-  mcp.setInputPolarity(MCP23017_PORTB, 0b01111111);
 }
