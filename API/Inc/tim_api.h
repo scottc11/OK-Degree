42d67c02fd7ebdeaaba7bfb69815cf4af152173a
scottc11 <scott.m.campbell11@gmail.com>
2022-01-14 11:00:30 -0500

    timer overflow calculator

--- API/Inc/tim_api.h
+++ API/Inc/tim_api.h
@@ -2,4 +2,4 @@
 
 #include "common.h"
 
-void tim_config(TIM_HandleTypeDef *htim, TIM_TypeDef *TIM, uint16_t prescaler, uint16_t period);
+uint32_t tim_get_overflow_freq(TIM_HandleTypeDef *htim);
--- API/Src/tim_api.cpp
+++ API/Src/tim_api.cpp
@@ -1,11 +1,24 @@
 #include "tim_api.h"
 
-
 /**
- * @brief Configure HAL TIM
- * Timer Overflow Frequency = APBx / ((prescaler + 1) * (period + 1))
-*/
-void tim_config(TIM_HandleTypeDef *htim, TIM_TypeDef *TIM, uint16_t prescaler, uint16_t period)
+ * @brief Get the freqeuncy of TIMx
+ * APBx / ((period + 1) * (prescaler + 1))
+ * @param TIM_HandleTypeDef
+ * @return uint32_t frequency
+ */
+uint32_t tim_get_overflow_freq(TIM_HandleTypeDef *htim)
 {
+    uint32_t pclk;
+    uint16_t prescaler = htim->Instance->PSC;
+    uint16_t period = __HAL_TIM_GET_AUTORELOAD(htim);
     
+    if (htim->Instance == TIM1)
+    {
+        pclk = HAL_RCC_GetPCLK1Freq();
+    } else {
+        pclk = HAL_RCC_GetPCLK2Freq();
+    }
+    uint32_t APBx_freq = pclk * 2; // Timer clocks are always equal to PCLK * 2
+
+    return APBx_freq / ((prescaler + 1)*(period + 1));
 }
