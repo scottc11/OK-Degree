ce30a84209cd9ef8360f77a51952b1d5f584baea
scottc11 <scott.m.campbell11@gmail.com>
2023-06-12 17:13:19 -0400

    finished writing the logger queue

--- Degree/Src/main.cpp
+++ Degree/Src/main.cpp
@@ -104,9 +104,8 @@ int main(void)
   multi_chan_adc_init();
   multi_chan_adc_start();
   HAL_Delay(100);
-  
 
-  xTaskCreate(TASK_logger, "logger", RTOS_STACK_SIZE_MIN, NULL, RTOS_PRIORITY_LOW, NULL);
+  xTaskCreate(task_logger, "logger", RTOS_STACK_SIZE_MIN, NULL, RTOS_PRIORITY_LOW, NULL);
   xTaskCreate(taskMain, "taskMain", 512, NULL, 1, &main_task_handle);
   xTaskCreate(task_controller, "controller", RTOS_STACK_SIZE_MIN, &glblCtrl, RTOS_PRIORITY_HIGH, NULL);
   xTaskCreate(task_interrupt_handler, "ISR handler", RTOS_STACK_SIZE_MIN, &glblCtrl, RTOS_PRIORITY_HIGH + 1, NULL);
